2e5a18d46fb5490a42122ac331da0484bc183a2c
Chris Davey <cd@cpd.id.au>
2012-10-27 15:02:36 +1000

    begin tinkering with functional graph will explore this post exams

--- au.id.cxd.Math/Graph.fs
+++ au.id.cxd.Math/Graph.fs
@@ -1,7 +1,35 @@
 ﻿namespace au.id.cxd.Collections
 
-type 'g Node = Node of 'g
-                |  EmptyNode
-                
-type 'g Edge = Edge of 'g * 'g
+open System
+open au.id.cxd.Collections.Stack
 
+module Graph =
+
+    // a node may contain data of type g and always has a guid
+    type 'g Node = Node of Guid * 'g
+    
+    /// generate a new node for data g
+    let makeNode g = Node (Guid.NewGuid(), g)
+    
+    /// an arc between two nodes
+    type Edge = Edge of Guid * Guid * float
+
+    /// make an edge between two nodes
+    let makeEdge (from:'g Node) (tonode:'g Node) (weight:float) =
+        let (Node (id1, g1)) = from
+        let (Node (id2, g2)) = tonode
+        (id1, id2, weight)
+    
+    /// a graph container
+    type 'g Graph = 
+        Graph of (Edge list * 'g Node * Edge list) list
+    
+    /// bootstrap a graph
+    let makeGraph rest = Graph rest
+    
+    /// extract all nodes in the graph
+    let extractNodes ((Graph graph):'g Graph) =
+        List.fold (fun st (inedges, node, outedges) -> node :: st) [] graph
+    
+    let depthFirst (visitFn:('a -> 'b -> unit)) (graph:'g Graph) =
+        ()
